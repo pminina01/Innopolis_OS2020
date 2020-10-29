00bf52772fa6530d2aa70f92bb4f724816f9e8ec
pminina01 <35624330+pminina01@users.noreply.github.com>
2020-10-29 19:16:41 +0300

    Update ex4.c

--- week11/ex4.c
+++ week11/ex4.c
@@ -9,15 +9,15 @@
 #include <sys/mman.h>
 
 int main () {
-  int f1 = open("ex1.txt", O_RDONLY);
+	int f1 = open("ex1.txt", O_RDONLY);
 	int f2 = open("ex1.memcp.txt", O_CREAT | O_RDWR);
 
 	struct stat buff;
-
-  char* s1 = mmap(NULL, buff.st_size, PROT_READ, MAP_SHARED, f1, 0);
+	
+	char* s1 = mmap(NULL, buff.st_size, PROT_READ, MAP_SHARED, f1, 0);
 	ftruncate(f2, buff.st_size);
 	char* s2 = mmap(NULL, buff.st_size, PROT_WRITE, MAP_SHARED, f2, 0);
-  memcpy(s2, s1, buff.st_size);
+	memcpy(s2, s1, buff.st_size);
 
 	munmap(s1, buff.st_size);
 	munmap(s2, buff.st_size);
